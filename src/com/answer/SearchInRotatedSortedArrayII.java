43a699455aef5749c3acecdca1c7acfedd4f1fc7
ice-melon <chunjiang2010@gmail.com>
2014-11-06 22:29:04 +0800

    work at home 2

--- src/com/answer/SearchInRotatedSortedArrayII.java
+++ src/com/answer/SearchInRotatedSortedArrayII.java
@@ -1,29 +1,31 @@
 package com.answer;
 
 /**
- * Created by chiang on 14-11-5.
+ * Created by chiang on 14/11/6.
  */
-public class SearchInRotatedSortedArray {
-    public static int search(int[] A, int target) {
+public class SearchInRotatedSortedArrayII {
+    public static boolean search(int[] A, int target) {
         int first = 0;
         int last = A.length - 1;
 
         while (first != (last + 1)) {
             int mid = (first + last) / 2;
             if(A[mid] == target){
-                return mid;
-            }else if (A[first] <= A[mid]) {
+                return true;
+            }else if (A[first] < A[mid]) {
                 if (A[first] <= target && target < A[mid])
                     last = mid;
                 else
                     first = mid + 1;
-            }else {
+            }else if (A[first] > A[mid]){
                 if (A[mid] < target && target <= A[last])
                     first = mid + 1;
                 else
                     last = mid;
+            }else {
+                first = first + 1;
             }
         }
-        return -1;
+        return false;
     }
 }
